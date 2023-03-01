5658d08e53894035afaa1fb7421d7d7e159189c0
yuhao <yuhao@mini1.cn>
2023-03-01 18:20:05 +0800

    [update] update go-log version

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/yuhao-jack/evolving-rpc
 
 go 1.19
 
-require github.com/yuhao-jack/go-toolx v0.0.0-20221223074924-19a3ae43a8da
+require github.com/yuhao-jack/go-toolx v0.0.0-20230301100358-980e6dc96056
 
 require github.com/yuhao-jack/go-log v0.0.0-20230228044707-fbeb64919627 // indirect
--- register_discover/register_discover_center.go
+++ register_discover/register_discover_center.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	evolvingserver "github.com/yuhao-jack/evolving-rpc/evolving-server"
 	"github.com/yuhao-jack/evolving-rpc/evolving-server/svr_mgr"
 	"github.com/yuhao-jack/evolving-rpc/model"
+	go_log "github.com/yuhao-jack/go-log"
 	"github.com/yuhao-jack/go-toolx/fun"
 	"net/http"
+	"os"
 )
 
 func main() {
-	serverConf := model.EvolvingServerConf{
-		BindHost:   "0.0.0.0",
-		ServerPort: 6601,
+	serverConf := model.EvolvingServerConf{}
+	var (
+		host         string
+		port, rdport int
+	)
+	ip, err := fun.GetLocalIp()
+	if err != nil {
+		go_log.GetSingleGoLog().Error("GetLocalIp err:%v", err)
+		os.Exit(1)
 	}
+	flag.StringVar(&serverConf.BindHost, "rdh", ip, "注册发现服务的host")
+	flag.IntVar(&rdport, "rdp", 6601, "注册发现服务的端口")
+	serverConf.ServerPort = int32(rdport)
+	flag.StringVar(&host, "h", ip, "工具服务host")
+	flag.IntVar(&port, "p", 8080, "工具服务端口")
+	flag.Parse()
+	go_log.GetSingleGoLog().Info("register and discover center addr:%s:%d", serverConf.BindHost, serverConf.ServerPort)
+	go_log.GetSingleGoLog().Info("tools service addr:%s:%d", host, port)
+
 	evolvingServer := evolvingserver.NewEvolvingServer(&serverConf)
 	go evolvingServer.Start()
 	handleMgr := HandleMgr{EvolvingServer: evolvingServer}
 	http.HandleFunc("/serviceInfoList", handleMgr.ServiceInfoList)
-	http.ListenAndServe("127.0.0.1:8000", nil)
+	http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), nil)
 }
 
 type HandleMgr struct {
